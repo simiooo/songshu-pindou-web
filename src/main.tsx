a5ae5ca8869a9f0b0925666325cdf01cacf4f585
Simooo <simooowann@gmail.com>
2026-03-20 20:39:48 +0900

    fix: 修改 vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ConfigProvider } from 'antd';
-import { Analytics } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
 import theme from './theme';
 import './i18n';
 import './index.css';
