22723c9ffae92b35bdadc41353b4439ffea4b4a4
Simooo <simooowann@gmail.com>
2026-03-20 20:11:55 +0900

    feat: integrate Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ConfigProvider } from 'antd';
+import { Analytics } from '@vercel/analytics/react';
 import theme from './theme';
 import './i18n';
 import './index.css';
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ConfigProvider theme={theme}>
       <App />
+      <Analytics />
     </ConfigProvider>
   </StrictMode>,
 );
